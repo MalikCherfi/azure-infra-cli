0979459e6b89db168add5dd656e099bf76da3dbe
malikcherfi <malikcherfi@gmail.com>
2026-06-25 14:14:19 +0200

    refactor: remove health check function and configuration

--- azure-function/HealthCheck/__init__.py
+++
@@ -1,8 +0,0 @@
-import azure.functions as func
-import json
-
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse(
-        json.dumps({"status": "healthy"}), mimetype="application/json", status_code=200
-    )

5a4a578058cdb108440ccedef274684163c96a30
malikcherfi <malikcherfi@gmail.com>
2026-06-25 14:50:24 +0200

    feat : update message

--- container-instances/index.php
+++ container-instances/index.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');
 echo json_encode([
-    "message" => "Hello from AzureTech ! Mon nom est " . gethostname(),
+    "message" => "Hello from AzureTech ! Mon nom est " . gethostname() . "bonne journée !",
     "service" => "Azure App Service (PaaS)",
     "runtime" => "PHP 8.2",
     "host"    => gethostname()
